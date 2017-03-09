98a3df5d0e25308b7b903920f32e42e8859d1b8d
Thorben B. Muehleder <thorbenm@physik.uni-kassel.de>
2017-03-09 15:26:58 +0100

    first try with a slider

--- lasercontrol_qt/mainwindow.cpp
+++ lasercontrol_qt/mainwindow.cpp
@@ -13,12 +13,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_on_button_clicked()
+void MainWindow::on_horizontalSlider_valueChanged(int value)
 {
-    transmit_voltage(current_to_voltage(45));
-}
-
-void MainWindow::on_off_button_clicked()
-{
-    transmit_voltage(current_to_voltage(0));
+    transmit_voltage(current_to_voltage(value));
 }
--- lasercontrol_qt/mainwindow.h
+++ lasercontrol_qt/mainwindow.h
@@ -38,6 +38,12 @@ private slots:
 
     void on_off_button_clicked();
 
+    void on_horizontalSlider_actionTriggered(int action);
+
+    void on_horizontalSlider_rangeChanged(int min, int max);
+
+    void on_horizontalSlider_valueChanged(int value);
+
 private:
     Ui::MainWindow *ui;
 };
